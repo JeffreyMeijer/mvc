4afb7798f44a40227c31aed957569772b69117f3
JeffreyMeijer <123974@student.drenthecollege.nl>
2020-03-10 09:54:05 +0100

    Opdracht 7

--- php/classes/mvc/App.class.php
+++ php/classes/mvc/App.class.php
@@ -4,9 +4,11 @@
   class App {
 
     private $view;
+    private $controller;
 
     public function __construct() {
-      $this->view = new \views\TestView(null,null);
+      $this->controller = new \controllers\TestController(null);
+      $this->view = new \views\TestView($this->controller,null);
     }
     public function __toString() {
       return $this->view->getHTML();
