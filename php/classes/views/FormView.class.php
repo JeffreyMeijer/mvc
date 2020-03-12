c57a39b4d63e9f6c5b8db1f4ba2a635a8a64ec37
JeffreyMeijer <123974@student.drenthecollege.nl>
2020-03-12 09:51:21 +0100

    Form inputs beveiligd

--- php/classes/views/FormView.class.php
+++ php/classes/views/FormView.class.php
@@ -3,14 +3,13 @@ namespace views;
 
 class FormView extends \mvc\View {
   public function getHTML() {
-    $voornaam = $this->controller->getGetData("voornaam");
-    $achternaam = $this->controller->getGetData("achternaam");
+    $voornaam = filter_var($this->controller->getGetData("voornaam"), FILTER_SANITIZE_STRING);
+    $achternaam = filter_var($this->controller->getGetData("achternaam"), FILTER_SANITIZE_STRING);
     $output = "<h1>Dit is de Form pagina</h1>";
     $output .= "<form method='GET'>";
     $output .= "<p>Voornaam: <input type='text' name='voornaam'></p>";
     $output .= "<p>Achternaam: <input type='text' name='achternaam'></p>";
     $output .= "<input type='submit' name='submit'>";
-    //$output .= "<a href=\"form?voornaam=$voornaam&achternaam=$achternaam\">Submit</a>";
     $output .= "</form>";
     $output .= "<br/>";
     $output .= "<h3>Voornaam: ". $voornaam ."</h3>";
